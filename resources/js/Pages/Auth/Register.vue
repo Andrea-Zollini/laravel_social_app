a77623015f8019c963cda6c03d642a5b2aa79d81
andrea <andrea.zollini@proton.me>
2024-02-05 17:22:03 +0100

    Gestione  username

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -41,6 +41,8 @@ const submit = () => {
                 <InputError class="mt-2" :message="form.errors.name" />
             </div>
 
+
+
             <div class="mt-4">
                 <InputLabel for="email" value="Email" />
 
